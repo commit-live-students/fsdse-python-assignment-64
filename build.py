800b6428febb72962e1a332f87f2baaa2f061f60
adinathauti <adinathauti@gmail.com>
2017-06-23 11:40:53 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,15 @@
+import numpy as np
+
 def solution(array):
-    """
-    Enter your code here
-    """
+    if(type(array)==list):
+        array.reverse()
+        return array
+    else:
+        arr2 = array.tolist()
+        arr2.reverse()
+        return arr2
+
+
+
+tp = solution(np.array([1,2,3,4,5,6]))
+print tp
