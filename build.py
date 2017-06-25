848688b9d4a01c3c18305b9de5ed91daf3802bd8
birajparikh16 <birajparikh16@gmail.com>
2017-06-25 19:17:17 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,11 @@
+import numpy as np
+a = np.arange(12,38)
+#print a
+
 def solution(array):
-    """
-    Enter your code here
-    """
+
+    return array[::-1]
+
+# solution(a)
+# Output :
+# [37 36 35 34 33 32 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12]]
